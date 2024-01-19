71b586b154a39e2d90158609f802c2a9e8becfd0
Joe Vincent <hellojustjoe@gmail.com>
2024-01-19 16:25:49 -0600

    DASHBOARD DONE AND WORKING

--- functions.py
+++ functions.py
@@ -9,6 +9,93 @@ import plotly.express as px
 import os
 import requests
 
+def get_feature_info(alcaldia_name=None):
+    alcaldias_info = {
+        'Alvaro Obregon': {
+            'Population': 759137,
+            'Area': 96.17,
+            'Population Density': 7893.70
+        },
+        'Azcapotzalco': {
+            'Population': 432205,
+            'Area': 33.66,
+            'Population Density': 12840.31
+        },
+        'Benito Juarez': {
+            'Population': 434153,
+            'Area': 26.63,
+            'Population Density': 16303.15
+        },
+        'Coyoacan': {
+            'Population': 614447,
+            'Area': 54.40,
+            'Population Density': 11294.98
+        },
+        'Cuajimalpa de Morelos': {
+            'Population': 217686,
+            'Area': 70.28,
+            'Population Density': 3097.41
+        },
+        'Cuauhtemoc': {
+            'Population': 545884,
+            'Area': 32.44,
+            'Population Density': 16827.50
+        },
+        'Gustavo A. Madero': {
+            'Population': 1173351,
+            'Area': 94.07,
+            'Population Density': 12473.17
+        },
+        'Iztacalco': {
+            'Population': 404695,
+            'Area': 23.30,
+            'Population Density': 17368.88
+        },
+        'Iztapalapa': {
+            'Population': 1835486,
+            'Area': 116.67,
+            'Population Density': 15732.29
+        },
+        'La Magdalena Contreras': {
+            'Population': 247622,
+            'Area': 74.58,
+            'Population Density': 3320.22
+        },
+        'Miguel Hidalgo': {
+            'Population': 414470,
+            'Area': 46.99,
+            'Population Density': 8820.39
+        },
+        'Milpa Alta': {
+            'Population': 152685,
+            'Area': 228.41,
+            'Population Density': 668.47
+        },
+        'Tlahuac': {
+            'Population': 392313,
+            'Area': 85.34,
+            'Population Density': 4597.06
+        },
+        'Tlalpan': {
+            'Population': 699928,
+            'Area': 312.00,
+            'Population Density': 2243.36
+        },
+        'Venustiano Carranza': {
+            'Population': 443704,
+            'Area': 33.42,
+            'Population Density': 13276.60
+        },
+        'Xochimilco': {
+            'Population': 442178,
+            'Area': 118.00,
+            'Population Density': 3747.27
+        }
+    }
+    if alcaldia_name is None:
+        return list(alcaldias_info.keys())
+    else:
+        return alcaldias_info.get(alcaldia_name, {'Population': 'Unknown', 'Area': 'Unknown', 'Description': 'No data available.'})
 
 API_HOST_LOCAL = os.getenv('API_HOST_LOCAL')
 
@@ -69,70 +156,102 @@ def show_historic_tvsf():
     cuenta_historica_binaria,
     labels=cuenta_historica_binaria.index.map({1: 'True Case Calls', 0: 'False Case Calls'}),
     values=cuenta_historica_binaria.values,
-    title='Histórico llamadas Verdaderas vs Incongruentes(2014-2023)',
+    #title='Histórico llamadas Verdaderas vs Incongruentes(2014-2023)',
     names=cuenta_historica_binaria.index.map({1: 'Casos Verdaderos', 0: 'Casos Incongruentes'}),
-    color_discrete_sequence=['blue', 'red'])
+    color_discrete_sequence=['blue', 'red']),
+    use_container_width=True
     )
 
 
-def show_predicted_incidents(predicted_data):
+def show_predicted_incidents(alcaldia_seleccionada):
     """
     plot predicted incident data in line and area charts
 
-    :param predicted_data: Dictionary containing month names as keys and predicted incident counts as values.
+    :param model_data: Dictionary containing month names as keys and predicted incident counts as values.
     """
 
-    print("Raw Predicted Data:", predicted_data)
 
-    if not isinstance(predicted_data, pd.DataFrame) or 'data' not in predicted_data:
-        print("Incorrect data format. 'predicted_data' should be a DataFrame with a 'data' column.")
-        return
+    alcaldia_info = get_feature_info(alcaldia_seleccionada.title())
+    population = alcaldia_info.get('Population', 1)  # Default to 1 to avoid division by zero
 
-    actual_data = predicted_data['data'].iloc[0]
-    if not isinstance(actual_data, dict):
-        print("The 'data' column does not contain a dictionary.")
-        return
 
-    # Convert the dictionary to a pandas DataFrame for easier manipulation
-    df = pd.DataFrame(list(actual_data.items()), columns=['Month', 'Predicted Incidents'])
-    # Convert 'Month' to a categorical type for proper sorting in the plot
-    print("DataFrame after conversion:", df.head())
 
-    df['Month'] = pd.Categorical(df['Month'], categories=[
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'],
-        ordered=True)
-    df.sort_values('Month', inplace=True)
-    print(df.head(10))
-    print(df.info())
-    # Create a line chart
+    params = {'name_alcaldia': alcaldia_seleccionada}
+    response = requests.get(API_HOST_LOCAL + '/model-data', params=params).json()
+    model_data = pd.DataFrame.from_dict(response)
+
+
+    months_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    model_data.index = pd.Categorical(model_data.index, categories=months_order, ordered=True)
+    df = model_data.sort_index(inplace=True)
+
+
+    # Line chart
     line_chart = go.Figure()
-    line_chart.add_trace(go.Scatter(x=df['Month'], y=df['Predicted Incidents'], mode='lines+markers',
-                                    name='Predicted Incidents', line=dict(color='blue', width=2)))
+    line_chart.add_trace(go.Scatter(x=model_data.index, y=model_data['data'], mode='lines+markers', name='Predicted Incidents', line=dict(color='blue', width=2)))
+    line_chart.update_layout(title='Monthly Predicted Incident Trends', xaxis_title='Month', yaxis_title='Number of Incidents', template='seaborn')
 
-    # Enhance chart layout
-    line_chart.update_layout(title='Monthly Predicted Incident Trends', xaxis_title='Month',
-                             yaxis_title='Number of Incidents', template='seaborn')
-
-    # Create an area chart
+    # Area chart
     area_chart = go.Figure()
-    area_chart.add_trace(go.Scatter(x=df['Month'], y=df['Predicted Incidents'], fill='tozeroy',
-                                    name='Incident Area', line=dict(color='cyan', width=2)))
+    area_chart.add_trace(go.Scatter(x=model_data.index, y=model_data['data'], fill='tozeroy', name='Incident Area', line=dict(color='cyan', width=2)))
+    area_chart.update_layout(title='Area Chart of Predicted Incidents', xaxis_title='Month', yaxis_title='Number of Incidents', template='seaborn')
 
-    # Enhance chart layout
-    area_chart.update_layout(title='Area Chart of Predicted Incidents', xaxis_title='Month',
-                             yaxis_title='Number of Incidents', template='seaborn')
+    print("modeldata : ", type(model_data), model_data)
+    print("pop variable : ", type(population), population)
+    model_data['crashes_per_capita'] = model_data['data'] / population
+
 
     # Pie chart
     pie_chart = go.Figure()
-    pie_chart.add_trace(go.Pie(labels=df['Month'], values=df['Predicted Incidents'],
-                               name='Incident Distribution'))
+    pie_chart.add_trace(go.Pie(labels=model_data.index, values=model_data['crashes_per_capita'], name='Crashes per Capita'))
+    pie_chart.update_layout(title='Predicted Crashes per Capita by Month', template='seaborn')
 
-    #make it sexy
-    pie_chart.update_layout(title='Distribution of Predicted Incidents by Month',
-                            template='seaborn')
-
+    return line_chart, pie_chart
     # Display charts in Streamlit
     #st.plotly_chart(line_chart)
-    st.plotly_chart(area_chart)
-    st.plotly_chart(pie_chart)
+    #st.plotly_chart(area_chart)
+    #st.plotly_chart(pie_chart)
+
+
+def show_aggregated_predictions():
+    # List of all alcaldías
+    print("test test")
+    alcaldias = get_feature_info()
+    total_predictions = {}
+    predictions_per_thousand = {}
+
+
+    for alcaldia in alcaldias:
+        params = {'name_alcaldia': alcaldia.upper()}
+        response = requests.get(API_HOST_LOCAL + '/model-data', params=params)
+        if response.status_code == 200:
+            model_data = pd.DataFrame.from_dict(response.json())
+            total = model_data['data'].sum()
+            total_predictions[alcaldia] = total
+
+            # Get population for the alcaldía
+            population_info = get_feature_info(alcaldia)
+            population = population_info.get('Population', 1)  # Default to 1 to avoid division by zero
+
+            # Calculate predicted incidents per 1000 people
+            predictions_per_thousand[alcaldia] = (total / population) * 1000
+        else:
+            print(f"Error fetching data for {alcaldia}: {response.status_code}")
+
+    total_prediction_df = pd.DataFrame(list(total_predictions.items()), columns=['Alcaldía', 'Total Predictions'])
+    predictions_per_thousand_df = pd.DataFrame(list(predictions_per_thousand.items()), columns=['Alcaldía', 'Predictions per 1000 People'])
+
+    # Sort the DataFrames
+    total_prediction_df = total_prediction_df.sort_values(by='Total Predictions', ascending=False)
+    predictions_per_thousand_df = predictions_per_thousand_df.sort_values(by='Predictions per 1000 People', ascending=False)
+
+    # Create and display the bar charts
+    total_bar_chart = px.bar(total_prediction_df, x='Alcaldía', y='Total Predictions',
+                             title='Total Predicted Incidents Distribution Among Alcaldías')
+    per_thousand_bar_chart = px.bar(predictions_per_thousand_df, x='Alcaldía', y='Predictions per 1000 People',
+                                    title='Predicted Incidents per 1000 People Among Alcaldías')
+
+    #st.plotly_chart(total_bar_chart, use_container_width=True)
+    #st.plotly_chart(per_thousand_bar_chart, use_container_width=True)
+
+    return total_bar_chart, per_thousand_bar_chart
--- testmain.py
+++ testmain.py
@@ -5,20 +5,33 @@ import pydeck as pdk
 import os
 import requests
 
-from functions import show_dynamic_plot, show_historic_tvsf, show_predicted_incidents
+from functions import show_dynamic_plot, show_historic_tvsf, show_predicted_incidents, get_feature_info, show_aggregated_predictions
 
 API_HOST_LOCAL = os.getenv('API_HOST_LOCAL')
+st.set_page_config(layout="wide")
 
 
 # Get main map
 response = requests.get(API_HOST_LOCAL + '/main-map')
 mapa = gpd.read_file(response.text, driver='GeoJSON')
 
+
 # Página principal
 def main():
-    st.markdown("# Consola de Datos 911 CDMX")
+    github_url = "https://github.com/wichoiscoding/"
+    badge_url = "https://img.shields.io/badge/-wichoiscoding-black?style=flat-square&logo=github"
 
-    st.markdown('### Obten informacion detallada acerca de los incidentes reportados al 911')
+    st.markdown(f"""
+        <div style="display: flex; justify-content: center; align-items: center;">
+            <h1 style="margin-right: 10px;">Consola de datos 911 CDMX</h1>
+            <a href="{github_url}">
+                <img src="{badge_url}" alt="GitHub Badge" style="height: 40px;">
+            </a>
+        </div>
+        """, unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'>Obten informacion detallada acerca de los incidentes reportados al 911</h3>", unsafe_allow_html=True)
+
+    #st.markdown('### Obten informacion detallada acerca de los incidentes reportados al 911')
 
     # name-alcaldia api call
     response = requests.get(API_HOST_LOCAL + '/name-alcaldia')
@@ -26,11 +39,13 @@ def main():
 
     # Select box alcaldia
     alcaldia_seleccionada = st.selectbox("Selecciona una alcaldía:", names_alcaldias)
-
+    col1, col2 = st.columns(2)
     # Get alcaldia lat & lon
     params = {'name_alcaldia': alcaldia_seleccionada}
     response = requests.get(API_HOST_LOCAL + '/latlon', params=params).json()
     latitud, longitud = response['Latitud'], response['Longitud']
+
+
 
     # Visualizar el mapa
     view_state = pdk.ViewState(
@@ -39,40 +54,52 @@ def main():
         zoom=12
     )
 
-    response = requests.get(API_HOST_LOCAL + '/model-data', params=params).json()
-    data_grouped = pd.DataFrame.from_dict([response])
+    #response = requests.get(API_HOST_LOCAL + '/model-data', params=params).json()
 
 
+    with col1:
+        st.markdown('## Mapa de CMDX')
 
-    layer_alcaldias = pdk.Layer(
-        "GeoJsonLayer",
-        data=mapa,
-        get_fill_color=[255, 0, 0, 100],
-        get_line_color=[0, 255, 0, 200],
-        get_line_width=60, #thicc so they can be seen
-        pickable=True,
-        auto_highlight=True,
-        opacity=0.8,
-        tooltip={
-                "text": "{NOMGEO}"
-        }
-    )
+        layer_alcaldias = pdk.Layer(
+            "GeoJsonLayer",
+            data=mapa,
+            get_fill_color=[255, 0, 0, 100],
+            get_line_color=[0, 255, 0, 200],
+            get_line_width=60, #thicc so they can be seen
+            pickable=True,
+            auto_highlight=True,
+            opacity=0.8,
+            tooltip={
+                    "text": "{NOMGEO}"
+            }
+        )
 
-    r = pdk.Deck(layers=[layer_alcaldias], initial_view_state=view_state, map_style=None)
+        r = pdk.Deck(layers=[layer_alcaldias], initial_view_state=view_state, map_style=None)
 
-    # Mostrar el mapa en Streamlit
-    st.pydeck_chart(r)
+        # Mostrar el mapa en Streamlit
+        st.pydeck_chart(r,use_container_width=True)
 
-    if st.button("Desplegar informacion Alcaldía mensual"):
-        # Navegar a la página de información detallada
-        st.session_state.ubicacion_seleccionada = alcaldia_seleccionada
-        st.experimental_rerun()
+        if st.button("Desplegar informacion Alcaldía mensual"):
+            # Navegar a la página de información detallada
+            st.session_state.ubicacion_seleccionada = alcaldia_seleccionada
+            st.experimental_rerun()
 
-    st.markdown('## Predicciones de Incidentes')
-    show_predicted_incidents(data_grouped)
+    line_chart, pie_chart = show_predicted_incidents(alcaldia_seleccionada)
+    #pie_chart = show_aggregated_predictions()
+    with col2:
+        st.markdown('## Predicciones de Incidentes')
+        st.plotly_chart(line_chart,use_container_width=True)
 
-    # Historic True vs False case calls
-    show_historic_tvsf()
+    col3, col4 = st.columns(2)
+    total_prediction, per_pop = show_aggregated_predictions()
+    with col3:
+        st.markdown('## Total incidentes por alcaldía')
+        st.plotly_chart(total_prediction,use_container_width=True)
+
+    with col4:
+        # Historic True vs False case calls
+        st.markdown('## Total incidentes por poblacion')
+        st.plotly_chart(per_pop,use_container_width=True)
 
 
     # Página de información detallada
